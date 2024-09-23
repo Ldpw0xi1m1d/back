babf4502f00f5358762c09c924ff08b8bb08ffcd
Ldpw0xi1m1d <dimas.batrakov.1995@bk.ru>
2024-09-23 19:49:18 +0300

    2nd commit

--- SimpleServer.java
+++ SimpleServer.java
@@ -20,18 +20,36 @@ public class SimpleServer {
     static class DataHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            try (InputStream requestBody = exchange.getRequestBody();
-                 ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = requestBody.read(buffer)) != -1) {
-                    output.write(buffer, 0, bytesRead);
+            exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().set("Access-Control-Request-Method", "*");
+            exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+            exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "*");
+            String method = exchange.getRequestMethod();
+            if ("POST".equals(method)) {
+                try (InputStream requestBody = exchange.getRequestBody();
+                     ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = requestBody.read(buffer)) != -1) {
+                        output.write(buffer, 0, bytesRead);
+                    }
+
+                    String text = output.toString();
+
+                    Files.write(Paths.get("info.txt"), text.getBytes());
+
+                    exchange.sendResponseHeaders(200, -1);
+                    OutputStream outputStream = exchange.getResponseBody();
+                    outputStream.close();
                 }
-                String text = output.toString();
-                Files.write(Paths.get("info.txt"), text.getBytes());
-                exchange.sendResponseHeaders(200, -1);
-                OutputStream outputStream = exchange.getResponseBody();
-                outputStream.close();
+            } else if ("GET".equals(method)) {
+                String data = new String(Files.readAllBytes(Paths.get("info.txt")));
+                exchange.sendResponseHeaders(200, data.getBytes().length);
+                try (OutputStream responseBody = exchange.getResponseBody()) {
+                    responseBody.write(data.getBytes());
+                }
+
             }
         }
     }
